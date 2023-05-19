654174ff4f9013034ae681d945cefeb0c8c960e7
Selena Cuadra <selenac99@hotmail.com>
2023-05-19 18:58:24 -0300

    Error en NavBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,17 +7,22 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import logo from '../assets/logoceramica.png';
 import { makeStyles } from '@mui/styles';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Badge } from '@mui/material';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     marginBottom: '7rem',
-    backgroundColor: 'green',
+    
+  
   },
   appBar: {
-    backgroundColor: 'blue',
+    backgroundColor: 'yellow',
+  
    
   },
+  
   grow: {
     flexGrow: 1,
   },
@@ -28,12 +33,13 @@ const useStyles = makeStyles((theme) => ({
     marginRight: '10px',
     height: '4rem',
   },
+ 
 }));
 
 export default function Navbar() {
   const classes = useStyles();
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <div className={classes.root}>
       <AppBar position="fixed" className={classes.appBar}>
         <Toolbar>
           <IconButton
@@ -41,23 +47,33 @@ export default function Navbar() {
             edge="start"
             color="inherit"
             aria-label="menu"
-            sx={{ mr: 2 }}
+            sx={{ mr: 2}}
             className={classes.menuButton}
           >
             <img src={logo} className={classes.image} alt="Logo" />
           </IconButton>
-          <Typography variant="h6" component="div" color="textPrimary">
-            hello guest
+          <div className={classes.grow}/>
+          <Typography variant="h6" component="p" color="textPrimary">
+            Hola perro
           </Typography>
           <div className={classes.button}>
-            <Button variant="filled">
+            <Button variant="contained">
             <strong>Sign in</strong>
             </Button>
-            {/* <shoppingCart fontSize="large" color="primary"/> */}
+
+            <IconButton aria-label='show cart items' color='inherit'>
+              <Badge badgeContent={2} color='secondary'>
+              <ShoppingCartIcon fontSize="large"/> 
+              </Badge>
+            
+            </IconButton>
+             
             
           </div>
         </Toolbar>
       </AppBar>
-    </Box>
+    
+    </div>
+      
   );
 }
