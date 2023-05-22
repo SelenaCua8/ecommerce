8d64e52edb9f5c4519b6d1a6d8ee6c1cbc1aa8f5
Selena Cuadra <selenac99@hotmail.com>
2023-05-22 13:04:27 -0300

    reemplazando switch

--- src/App.js
+++ src/App.js
@@ -195,10 +195,69 @@
 
 // export default App;
 
+// import './App.css';
+// import React, { useEffect } from 'react';
+// import { ThemeProvider, createTheme } from '@mui/material/styles';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import Navbar from './components/Navbar';
+// import Product from './components/Product';
+// import Products from './components/Products';
+// import CheckoutPage from './components/CheckoutPage';
+// import CheckoutCard from './components/CheckoutCard';
+// import SignIn from './components/Signin';
+// import SignUp from './components/Signup';
+// import { auth } from './firebase';
+// import { useStateValue } from './StateProvider';
+// import Checkout from './CheckoutForm/Checkout';
+// import { actionTypes } from './reducer';
+
+// const theme = createTheme();
+
+// function App() {
+//   const [{ user }, dispatch] = useStateValue();
+
+//   useEffect(() => {
+//     const unsubscribe = auth.onAuthStateChanged((authUser) => {
+//       console.log(authUser);
+//       if (authUser) {
+//         dispatch({
+//           type: actionTypes.SET_USER,
+//           user: authUser,
+//         });
+//       }
+//     });
+
+//     return () => {
+//       unsubscribe();
+//     };
+//   }, [dispatch]);
+
+//   return (
+//     <Router>
+//       <ThemeProvider theme={theme}>
+//         <div className="App">
+//           <div className="container">
+//             <Navbar />
+//             <Switch>
+//               <Route path="/signup" component={SignUp} />
+//               <Route path="/signin" component={SignIn} />
+//               <Route path="/checkout-page" component={CheckoutPage} />
+//               <Route path="/checkout" component={Checkout} />
+//               <Route path="/" component={Products} />
+//             </Switch>
+//           </div>
+//         </div>
+//       </ThemeProvider>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
 import './App.css';
 import React, { useEffect } from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Product from './components/Product';
 import Products from './components/Products';
@@ -238,13 +297,13 @@ function App() {
         <div className="App">
           <div className="container">
             <Navbar />
-            <Switch>
-              <Route path="/signup" component={SignUp} />
-              <Route path="/signin" component={SignIn} />
-              <Route path="/checkout-page" component={CheckoutPage} />
-              <Route path="/checkout" component={Checkout} />
-              <Route path="/" component={Products} />
-            </Switch>
+            <Routes>
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/signin" element={<SignIn />} />
+              <Route path="/checkout-page" element={<CheckoutPage />} />
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="/" element={<Products />} />
+            </Routes>
           </div>
         </div>
       </ThemeProvider>
